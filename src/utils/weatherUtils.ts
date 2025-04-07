6ca877df010ee8445e13a522dd563f376ad89f2e
kreso90 <kresimir.selendic@vivelacar.com>
2025-04-07 14:09:33 +0200

    change category

--- src/utils/weatherUtils.ts
+++ src/utils/weatherUtils.ts
@@ -45,8 +45,8 @@ export const getNextHours = (currentHour: number, currentHours: Hour[], totalHou
 
 export const getWeatherClass = (weatherCode: number): string | undefined => {
     const weatherCategories = {
-        sunny: [1000],
-        cloudy: [1006, 1009, 1030, 1135, 1147, 1003],
+        sunny: [1000, 1003],
+        cloudy: [1006, 1009, 1030, 1135, 1147],
         rainy: [1063, 1072, 1150, 1153, 1168, 1171, 1180, 1183, 1186, 1189, 1192, 1195, 1198, 1201, 1240, 1243, 1246, 1273, 1276],
         snow: [1066, 1069, 1114, 1117, 1168, 1171, 1204, 1207, 1210, 1213, 1216, 1219, 1222, 1225, 1237, 1255, 1258, 1261, 1264, 1279, 1282],
     };
